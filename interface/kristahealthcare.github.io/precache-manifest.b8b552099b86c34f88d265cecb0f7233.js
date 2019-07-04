33f703ec74e8453cdf9bc814275f8ceefab819c4
mathcode00 <bruno.m.cebola@gmail.com>
2019-07-04 16:19:21 +0100

    initial commit

---
+++ interface/kristahealthcare.github.io/precache-manifest.b8b552099b86c34f88d265cecb0f7233.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f109fff5dbbd023fcc048e86d035fd15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdd7830ede09102ed9c7",
+    "url": "/static/css/2.5ec480d0.chunk.css"
+  },
+  {
+    "revision": "0356520c44db8baf46df",
+    "url": "/static/css/main.ec911bf5.chunk.css"
+  },
+  {
+    "revision": "cdd7830ede09102ed9c7",
+    "url": "/static/js/2.a4215349.chunk.js"
+  },
+  {
+    "revision": "0356520c44db8baf46df",
+    "url": "/static/js/main.ee67a649.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bebb9c004a2d0f83680bb6ef2e5962c6",
+    "url": "/static/media/icon.bebb9c00.png"
+  },
+  {
+    "revision": "4788048f319dc48101678d9e69f5077e",
+    "url": "/static/media/user.4788048f.png"
+  }
+]);
--- interface/src/services/api.js
+++ interface/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:3001/kristaApi/'});
+const api = axios.create({ baseURL: 'https://kristaapi.herokuapp.com/kristaApi/'});
 
 export default api;
